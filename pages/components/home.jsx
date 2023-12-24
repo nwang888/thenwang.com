3c8b53acb1189551838b33ae2e69183de7c4ca02
nwang888 <nwang888@gmail.com>
2023-12-23 23:11:48 -0800

    started projects page

---
+++ pages/components/home.jsx
@@ -0,0 +1,85 @@
+import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import NavBar from "./navbar";
+
+export default function Home() {
+	const [verb, setVerb] = useState("build");
+	useEffect(() => {
+		const verbs = ["imagine", "design", "build"];
+		let index = 0;
+
+		const intervalId = setInterval(() => {
+			index = index + 1 === verbs.length ? 0 : index + 1;
+			setVerb(verbs[index]);
+		}, 10000); // Change text every 10 seconds
+
+		return () => clearInterval(intervalId); // Clear interval on component unmount
+	}, []);
+	return (
+		<>
+			<div className="home-bg">
+				<div className="home-wrapper">
+					<div className="home-outer">
+						<div className="home-inner">
+							<div className="home-nameplate">
+								<div className="name">N Wang</div>
+								<div className="title">Strategist | Builder | Orchestrator</div>
+							</div>
+							<hr className="home-hr" />
+							<p>
+								I’m currently a junior at Duke University studying Computer
+								Science and Economics. I aim to find a way to the world where
+								individuals can self-identify and improve their own habits and
+								behaviors through novel and user-friendly development.
+							</p>
+							<div>
+								<p>
+									Let's <span className="animated-text">{verb}</span> together!
+								</p>
+							</div>
+							<ul className="home-links">
+								<motion.a
+									href="https://github.com/nwang888"
+									target="_blank"
+									rel="noopener noreferrer"
+									whileHover={{
+										scale: 1.3
+										// rotate: 360,
+									}}
+									transition={{ type: "spring", stiffness: 400, damping: 20 }}
+								>
+									<img src="/GithubButton.svg" alt="Github" />
+								</motion.a>
+								<motion.a
+									href="https://www.linkedin.com/in/nwang888/"
+									target="_blank"
+									rel="noopener noreferrer"
+									whileHover={{
+										scale: 1.3
+										// rotate: 360,
+									}}
+									transition={{ type: "spring", stiffness: 400, damping: 20 }}
+								>
+									<img src="/LinkedinButton.svg" alt="LinkedIn" />
+								</motion.a>
+								<motion.a
+									href="https://www.twitter.com/nwang888/"
+									target="_blank"
+									rel="noopener noreferrer"
+									whileHover={{
+										scale: 1.3
+										// rotate: 360,
+									}}
+									transition={{ type: "spring", stiffness: 400, damping: 20 }}
+								>
+									<img src="/TwitterButton.svg" alt="LinkedIn" />
+								</motion.a>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<NavBar />
+			</div>
+		</>
+	);
+}
---
+++ pages/components/projects.jsx
@@ -0,0 +1,20 @@
+export default function Projects() {
+	return (
+		<>
+			<div className="project1-bg">
+				<div className="project1-card"></div>
+				<div className="project1-desc">
+					<div className="project1-title">BetterMessages</div>
+					<hr className="project1-hr" />
+					<div className="project1-text">
+						<p>
+							BetterMessages is a Chrome extension that allows users to
+							automatically send messages to their friends and family members.
+							The extension is currently in development.
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
